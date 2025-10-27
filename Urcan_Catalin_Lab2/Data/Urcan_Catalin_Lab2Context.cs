ae042a4816a163fb8e1419b52b709115861572fa
catalin <catalin.urcan@gmail.com>
2025-10-27 15:44:58 +0200

    Add Author

--- Urcan_Catalin_Lab2/Data/Urcan_Catalin_Lab2Context.cs
+++ Urcan_Catalin_Lab2/Data/Urcan_Catalin_Lab2Context.cs
@@ -16,5 +16,6 @@ namespace Urcan_Catalin_Lab2.Data
 
         public DbSet<Urcan_Catalin_Lab2.Models.Book> Book { get; set; } = default!;
         public DbSet<Urcan_Catalin_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+        public DbSet<Urcan_Catalin_Lab2.Models.Author> Author { get; set; } = default!;
     }
 }
--- Urcan_Catalin_Lab2/Pages/Authors/Index.cshtml.cs
+++ Urcan_Catalin_Lab2/Pages/Authors/Index.cshtml.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Urcan_Catalin_Lab2.Data;
 using Urcan_Catalin_Lab2.Models;
 
-namespace Urcan_Catalin_Lab2.Pages.Books
+namespace Urcan_Catalin_Lab2.Pages.Authors
 {
     public class IndexModel : PageModel
     {
@@ -19,11 +19,11 @@ namespace Urcan_Catalin_Lab2.Pages.Books
             _context = context;
         }
 
-        public IList<Book> Book { get;set; } = default!;
+        public IList<Author> Author { get;set; } = default!;
 
         public async Task OnGetAsync()
         {
-            Book = await _context.Book.Include(b => b.Publisher).Include(a => a.Author).ToListAsync();
+            Author = await _context.Author.ToListAsync();
         }
     }
 }
