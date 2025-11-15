6820f034db8e4b43cfdefa4d4585c15e4d41939f
catalin <catalin.urcan@gmail.com>
2025-11-15 09:16:17 +0200

    Dysplay author

--- Urcan_Catalin_Lab2/Pages/Books/Index.cshtml.cs
+++ Urcan_Catalin_Lab2/Pages/Books/Index.cshtml.cs
@@ -27,7 +27,7 @@ namespace Urcan_Catalin_Lab2.Pages.Books
         {
             BookD = new BookData();
 
-            BookD.Books = await _context.Book
+            BookD.Books = await _context.Book.Include(b=>b.Author)
             .Include(b => b.Publisher)
             .Include(b => b.BookCategories)
             .ThenInclude(b => b.Category)
