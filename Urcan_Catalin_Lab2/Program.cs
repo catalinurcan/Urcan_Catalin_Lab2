e2aa57dd4c2e67a4eb3d3c533306f96f49b0601e
catalin <catalin.urcan@gmail.com>
2025-11-22 10:28:57 +0200

    Update

--- Urcan_Catalin_Lab2/Program.cs
+++ Urcan_Catalin_Lab2/Program.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Urcan_Catalin_Lab2.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +9,12 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Urcan_Catalin_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Urcan_Catalin_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Urcan_Catalin_Lab2Context' not found.")));
 
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("Urcan_Catalin_Lab2Context") ?? throw new InvalidOperationException("Connectionstring 'Urcan_Catalin_Lab2Context' not found.")));
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+options.SignIn.RequireConfirmedAccount = true)
+ .AddEntityFrameworkStores<LibraryIdentityContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
