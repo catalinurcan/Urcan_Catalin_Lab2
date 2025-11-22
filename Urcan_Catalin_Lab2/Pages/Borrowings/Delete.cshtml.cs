87b35b8fe558ff6fe533dd3dab9c1d8f7a4bf11f
catalin <catalin.urcan@gmail.com>
2025-11-22 11:01:59 +0200

    Details-

--- Urcan_Catalin_Lab2/Pages/Borrowings/Delete.cshtml.cs
+++ Urcan_Catalin_Lab2/Pages/Borrowings/Delete.cshtml.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using Urcan_Catalin_Lab2.Data;
 using Urcan_Catalin_Lab2.Models;
 
@@ -38,7 +39,18 @@ namespace Urcan_Catalin_Lab2.Pages.Borrowings
             else
             {
                 Borrowing = borrowing;
+            //    var bookList = _context.Book
+            //.Include(b => b.Author)
+            //.Select(x => new
+            //{
+            //    x.ID,
+            //    BookFullName = x.Title + " - " + x.Author.LastName + " " + x.Author.FirstName
+            //});
+
+            //    ViewData["BookID"] = new SelectList(bookList, "ID", "BookFullName");
+            //    ViewData["MemberID"] = new SelectList(_context.Member, "ID", "FullName");
             }
+
             return Page();
         }
 
